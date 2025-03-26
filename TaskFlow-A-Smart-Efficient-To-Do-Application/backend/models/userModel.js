fa9b67edef9c68fd6442351160dc87497703eeb8
karanudani3011 <karanudani30@gmail.com>
2025-03-26 13:06:38 +0530

    Update userModel.js

--- TaskFlow-A-Smart-Efficient-To-Do-Application/backend/models/userModel.js
+++ TaskFlow-A-Smart-Efficient-To-Do-Application/backend/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoos = require("mongoose")
 //Create .env file and t=store the user in side it or replace "Your MongoDB URL" with your url
-mongoos.connect("mongodb+srv://100xDev:i0XUrTfJXBtKTdk4@cluster0.9uh7q.mongodb.net/NewTODO")
+mongoos.connect("")
 
 const userSchema = mongoos.Schema({
     username: String,
